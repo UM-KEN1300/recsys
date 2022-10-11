afce500daab06ec34bb5ea17d149295f762eba20
Dino <dpasic8@gmail.com>
2022-10-11 14:59:55 +0200

    ratings system for songs in other playlists fully implemented

--- groups/aggregation.py
+++ groups/aggregation.py
@@ -1,6 +1,7 @@
 import json
+import math
 import compute_ratings
-from similarity import cosine_similarity
+
 print("Loading tracks and playlists..")
 with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Tracks.json", encoding="utf8") as f:
     TRACKS = json.load(f)
@@ -10,9 +11,23 @@ with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Playlists_small
 print('Tracks and playlists loaded!')
 
 playlist_1 = PLAYLISTS[0]['tracks']
+print("Playlist 1, " + PLAYLISTS[0]['name'] + " added, with " + str(len(playlist_1)) + " songs.")
 playlist_2 = PLAYLISTS[1]['tracks']
+print("Playlist 2, " + PLAYLISTS[1]['name'] + " added, with " + str(len(playlist_2)) + " songs.")
 playlist_3 = PLAYLISTS[2]['tracks']
+print("Playlist 3, " + PLAYLISTS[2]['name'] + " added, with " + str(len(playlist_3)) + " songs.")
 playlist_4 = PLAYLISTS[3]['tracks']
+print("Playlist 4, " + PLAYLISTS[3]['name'] + " added, with " + str(len(playlist_4)) + " songs.")
 
-other_ratings_playlist_1, other_ratings_playlist_2, other_ratings_playlist_3, other_ratings_playlist_4 = compute_ratings.compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4)
-print(other_ratings_playlist_1, other_ratings_playlist_2, other_ratings_playlist_3, other_ratings_playlist_4)
+tracks = playlist_1 + playlist_2 + playlist_3 + playlist_4
+
+
+target_size = math.floor((len(playlist_1) + len(playlist_2) + len(playlist_3) + len(playlist_4))/4)
+print("Target playlist will contain " + str(target_size) + " songs.")
+
+
+ratings_playlist_1, ratings_playlist_2, ratings_playlist_3, ratings_playlist_4 = compute_ratings.compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4)
+print(ratings_playlist_1)
+
+
+
--- groups/compute_ratings.py
+++ groups/compute_ratings.py
@@ -1,5 +1,16 @@
+import json
 from similarity import cosine_similarity
+print("Loading tracks and playlists..")
+with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Tracks.json", encoding="utf8") as f:
+    TRACKS = json.load(f)
+#Maybe we can get a smaller version of this file cause we only really need a couple playlists
+with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Playlists_small.json", encoding="utf8") as f:
+    PLAYLISTS = json.load(f)
+print('Tracks and playlists loaded!')
 
+def add_own_songs(playlist, ratings):
+    for i in range(len(playlist)):
+        ratings.update({playlist[i]: 1})
 def compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4):
     playlists = [playlist_1, playlist_2, playlist_3, playlist_4]
     other_ratings_playlist_1 = {}
@@ -7,6 +18,10 @@ def compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4):
     other_ratings_playlist_3 = {}
     other_ratings_playlist_4 = {}
 
+    add_own_songs(playlist_1, other_ratings_playlist_1)
+    add_own_songs(playlist_2, other_ratings_playlist_2)
+    add_own_songs(playlist_3, other_ratings_playlist_3)
+    add_own_songs(playlist_4, other_ratings_playlist_4)
     for i in range(1, 4):
         playlist = playlists[i]
         for j in range(len(playlist)):
@@ -34,10 +49,14 @@ def compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4):
                 other_ratings_playlist_3.update({playlist[j]: (similarity_sum / len(playlist_3))})
 
     for i in range(0, 3):
+        if i != 2:
             playlist = playlists[i]
             for j in range(len(playlist)):
                 similarity_sum = 0
                 for k in range(len(playlist_4)):
                     similarity_sum += cosine_similarity.cosine_similarity_by_uri(playlist_4[k], playlist[j])
                 other_ratings_playlist_4.update({playlist[j]: (similarity_sum / len(playlist_4))})
+
+
+
     return other_ratings_playlist_1, other_ratings_playlist_2, other_ratings_playlist_3, other_ratings_playlist_4
