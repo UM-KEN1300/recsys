bed91aeb5d7e1cef643b6bdbce61a6c664efdb99
Dino <dpasic8@gmail.com>
2022-10-19 23:39:31 +0200

    .

--- groups/aggregation.py
+++ groups/aggregation.py
@@ -1,9 +1,9 @@
 import json
 from similarity import compute_ratings
 import operator
-
+import random
 from explanations import explanations
-
+import numpy as np
 print("Loading tracks and playlists..")
 with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Tracks.json", encoding="utf8") as f:
     TRACKS = json.load(f)
@@ -12,85 +12,206 @@ with open("D:/Users/Dino/Recommender Systems/Project/recsys/data/Playlists_small
     PLAYLISTS = json.load(f)
 print('Tracks and playlists loaded!')
 
+
 #Load in any 4 playlist to base recommendations on
-playlist_1 = PLAYLISTS[0]['tracks']
-print("Playlist 1, " + PLAYLISTS[0]['name'] + " added, with " + str(len(playlist_1)) + " songs.")
-playlist_2 = PLAYLISTS[1]['tracks']
-print("Playlist 2, " + PLAYLISTS[1]['name'] + " added, with " + str(len(playlist_2)) + " songs.")
-playlist_3 = PLAYLISTS[2]['tracks']
-print("Playlist 3, " + PLAYLISTS[2]['name'] + " added, with " + str(len(playlist_3)) + " songs.")
-playlist_4 = PLAYLISTS[3]['tracks']
-print("Playlist 4, " + PLAYLISTS[3]['name'] + " added, with " + str(len(playlist_4)) + " songs.")
-tracks = playlist_1 + playlist_2 + playlist_3 + playlist_4
+def aggregate(indices):
+    index_1, index_2, index_3, index_4 = indices
+    playlist_1 = PLAYLISTS[index_1]['tracks']
+    print("Playlist 1, " + PLAYLISTS[0]['name'] + " added, with " + str(len(playlist_1)) + " songs.")
+    playlist_2 = PLAYLISTS[index_2]['tracks']
+    print("Playlist 2, " + PLAYLISTS[1]['name'] + " added, with " + str(len(playlist_2)) + " songs.")
+    playlist_3 = PLAYLISTS[index_3]['tracks']
+    print("Playlist 3, " + PLAYLISTS[2]['name'] + " added, with " + str(len(playlist_3)) + " songs.")
+    playlist_4 = PLAYLISTS[index_4]['tracks']
+    print("Playlist 4, " + PLAYLISTS[3]['name'] + " added, with " + str(len(playlist_4)) + " songs.")
+    tracks = playlist_1 + playlist_2 + playlist_3 + playlist_4
+    playlist_list = [playlist_1, playlist_2, playlist_3, playlist_4]
+    indices = [index_1,index_2,index_3,index_4]
+    index = 0
+    for playlist in playlist_list:
+        print("playlist " + str(indices[index]) + "////////////////////////////////////////////////////")
+        for song in playlist:
+            print(TRACKS[song]['track_name'])
+        index+=1
+    #Size of the final playlist
+    target_size = 30
+    print("Target playlist will contain " + str(target_size) + " songs.")
 
-#Size of the final playlist
-target_size = 50
-print("Target playlist will contain " + str(target_size) + " songs.")
-
-#Compute all estimated ratings of songs that are not in the initial playlist based on cosine similarity of certain features fetched from the spotify API
-ratings_playlist_1, ratings_playlist_2, ratings_playlist_3, ratings_playlist_4 = compute_ratings.compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4)
+    #Compute all estimated ratings of songs that are not in the initial playlist based on cosine similarity of certain features fetched from the spotify API
+    ratings_playlist_1, ratings_playlist_2, ratings_playlist_3, ratings_playlist_4 = compute_ratings.compute_ratings(playlist_1, playlist_2, playlist_3, playlist_4)
 
 
-print("AVERAGE: ")
-#AVERAGE GROUP RATING aggregation approach
-ratings_AVG = {}
-for i in range(len(tracks)):
-    uri = tracks[i]
-    rating_user_1 = ratings_playlist_1[uri]
-    rating_user_2 = ratings_playlist_2[uri]
-    rating_user_3 = ratings_playlist_3[uri]
-    rating_user_4 = ratings_playlist_4[uri]
-    final_rating = (rating_user_1+rating_user_2+rating_user_3+rating_user_4)/4
-    ratings_AVG.update({uri:final_rating})
-ratings_AVG = sorted(ratings_AVG.items(), key=operator.itemgetter(1), reverse=True)
+    print("AVERAGE: ")
+    #AVERAGE GROUP RATING aggregation approach
+    ratings_AVG = {}
+    for i in range(len(tracks)):
+        uri = tracks[i]
+        rating_user_1 = ratings_playlist_1[uri]
+        rating_user_2 = ratings_playlist_2[uri]
+        rating_user_3 = ratings_playlist_3[uri]
+        rating_user_4 = ratings_playlist_4[uri]
+        final_rating = (rating_user_1+rating_user_2+rating_user_3+rating_user_4)/4
+        ratings_AVG.update({uri:final_rating})
+    ratings_AVG = sorted(ratings_AVG.items(), key=operator.itemgetter(1), reverse=True)
 
-final_playlist_AVG = []
-for i in range(target_size):
-    print("Song " + str(i+1) + ": " + TRACKS[ratings_AVG[i][0]]['track_name'] + " with a rating of " + str(ratings_AVG[i][1]) + ".")
-    final_playlist_AVG.append(ratings_AVG[i][0])
+    final_playlist_AVG = []
+    for i in range(target_size):
+        print("Song " + str(i+1) + ": " + TRACKS[ratings_AVG[i][0]]['track_name'] + " with a rating of " + str(ratings_AVG[i][1]) + ".")
+        final_playlist_AVG.append(ratings_AVG[i][0])
 
-#Print explanations for all songs that are in the final playlist but not in the user's playlist
-for song in final_playlist_AVG:
-    user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
-    min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
-    user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4
-    explanations.explanation_groups_AVG(playlist_1, final_playlist_AVG, song, user_that_likes_song, user_that_dislikes_song)
+    #Print explanations for all songs that are in the final playlist but not in the user's playlist
+    for song in final_playlist_AVG:
+        user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
+        min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
+        user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4
+        explanations.explanation_groups_AVG(playlist_1, final_playlist_AVG, song, user_that_likes_song, user_that_dislikes_song)
 
-#Print explanations for all songs that are not in the final playlist but are in the user's playlist
-for song in playlist_1:
-    user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
-    min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
-    user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4 if min_rating == ratings_playlist_4[song] else 0
-    explanations.explanation_groups_AVG(playlist_1, final_playlist_AVG, song, user_that_likes_song, user_that_dislikes_song)
+    #Print explanations for all songs that are not in the final playlist but are in the user's playlist
+    for song in playlist_1:
+        user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
+        min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
+        user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4 if min_rating == ratings_playlist_4[song] else 0
+        explanations.explanation_groups_AVG(playlist_1, final_playlist_AVG, song, user_that_likes_song, user_that_dislikes_song)
 
-print("LEAST MISERY: ")
-#LEAST MISERY aggregation approach
-ratings_LEAST_MISERY = {}
-for i in range(len(tracks)):
-    uri = tracks[i]
-    rating_user_1 = ratings_playlist_1[uri]
-    rating_user_2 = ratings_playlist_2[uri]
-    rating_user_3 = ratings_playlist_3[uri]
-    rating_user_4 = ratings_playlist_4[uri]
-    final_rating = min([rating_user_1, rating_user_2, rating_user_3, rating_user_4])
-    ratings_LEAST_MISERY.update({uri:final_rating})
-ratings_LEAST_MISERY = sorted(ratings_LEAST_MISERY.items(), key=operator.itemgetter(1), reverse=True)
+    print("LEAST MISERY: ")
+    #LEAST MISERY aggregation approach
+    ratings_LEAST_MISERY = {}
+    for i in range(len(tracks)):
+        uri = tracks[i]
+        rating_user_1 = ratings_playlist_1[uri]
+        rating_user_2 = ratings_playlist_2[uri]
+        rating_user_3 = ratings_playlist_3[uri]
+        rating_user_4 = ratings_playlist_4[uri]
+        final_rating = min([rating_user_1, rating_user_2, rating_user_3, rating_user_4])
+        ratings_LEAST_MISERY.update({uri:final_rating})
+    ratings_LEAST_MISERY = sorted(ratings_LEAST_MISERY.items(), key=operator.itemgetter(1), reverse=True)
 
-final_playlist_LEAST_MISERY = []
-for i in range(target_size):
-    print("Song " + str(i+1) + ": " + TRACKS[ratings_LEAST_MISERY[i][0]]['track_name'] + " with a rating of " + str(ratings_LEAST_MISERY[i][1]) + ".")
-    final_playlist_LEAST_MISERY.append(ratings_LEAST_MISERY[i][0])
+    final_playlist_LEAST_MISERY = []
+    for i in range(target_size):
+        print("Song " + str(i+1) + ": " + TRACKS[ratings_LEAST_MISERY[i][0]]['track_name'] + " with a rating of " + str(ratings_LEAST_MISERY[i][1]) + ".")
+        final_playlist_LEAST_MISERY.append(ratings_LEAST_MISERY[i][0])
 
-#Print explanations for all songs that are in the final playlist but not in the user's playlist
-for song in final_playlist_LEAST_MISERY:
-    user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
-    min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
-    user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4
-    explanations.explanation_groups_LM(playlist_1, final_playlist_LEAST_MISERY, song, user_that_likes_song, user_that_dislikes_song)
+    #Print explanations for all songs that are in the final playlist but not in the user's playlist
+    for song in final_playlist_LEAST_MISERY:
+        user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
+        min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
+        user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4
+        explanations.explanation_groups_LM(playlist_1, final_playlist_LEAST_MISERY, song, user_that_likes_song, user_that_dislikes_song)
 
-#Print explanations for all songs that are not in the final playlist but are in the user's playlist
-for song in playlist_1:
-    user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
-    min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
-    user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4 if min_rating == ratings_playlist_4[song] else 0
-    explanations.explanation_groups_LM(playlist_1, final_playlist_LEAST_MISERY, song, user_that_likes_song, user_that_dislikes_song)
+    #Print explanations for all songs that are not in the final playlist but are in the user's playlist
+    for song in playlist_1:
+        user_that_likes_song = 1 if song in playlist_1 else 2 if song in playlist_2 else 3 if song in playlist_3 else 4
+        min_rating = min(ratings_playlist_1[song], ratings_playlist_2[song], ratings_playlist_3[song], ratings_playlist_4[song])
+        user_that_dislikes_song = 1 if min_rating == ratings_playlist_1[song] else 2 if min_rating == ratings_playlist_2[song] else 3 if min_rating == ratings_playlist_3[song] else 4 if min_rating == ratings_playlist_4[song] else 0
+        explanations.explanation_groups_LM(playlist_1, final_playlist_LEAST_MISERY, song, user_that_likes_song, user_that_dislikes_song)
+
+    songs_in_playlist_1 = 0
+    songs_in_playlist_2 = 0
+    songs_in_playlist_3 = 0
+    songs_in_playlist_4 = 0
+    for song in final_playlist_AVG:
+        if song in playlist_1:
+            songs_in_playlist_1+=1
+        if song in playlist_2:
+            songs_in_playlist_2+=1
+        if song in playlist_3:
+            songs_in_playlist_3+=1
+        if song in playlist_4:
+            songs_in_playlist_4+=1
+
+    print("AVERAGE:")
+    print("Songs in final playlist from playlist 1: " + str(songs_in_playlist_1) + " from original " + str(len(playlist_1)))
+    print("Songs in final playlist from playlist 2: " + str(songs_in_playlist_2) + " from original " + str(len(playlist_2)))
+    print("Songs in final playlist from playlist 3: " + str(songs_in_playlist_3) + " from original " + str(len(playlist_3)))
+    print("Songs in final playlist from playlist 4: " + str(songs_in_playlist_4) + " from original " + str(len(playlist_4)))
+
+    print("Average similarity of chosen songs for user 1: ")
+    similarity_user_1_sum = 0
+    similarity_user_2_sum = 0
+    similarity_user_3_sum = 0
+    similarity_user_4_sum = 0
+    for song in final_playlist_AVG:
+        similarity_user_1_sum += ratings_playlist_1[song]
+        similarity_user_2_sum += ratings_playlist_2[song]
+        similarity_user_3_sum += ratings_playlist_3[song]
+        similarity_user_4_sum += ratings_playlist_4[song]
+    print("Average expected rating user 1: " + str(similarity_user_1_sum/target_size))
+    print("Average expected rating user 2: " + str(similarity_user_2_sum/target_size))
+    print("Average expected rating user 3: " + str(similarity_user_3_sum/target_size))
+    print("Average expected rating user 4: " + str(similarity_user_4_sum/target_size))
+
+    list_nums_AVG = [songs_in_playlist_1, songs_in_playlist_2, songs_in_playlist_3, songs_in_playlist_4]
+    list_ratings_AVG = [similarity_user_1_sum/target_size,similarity_user_2_sum/target_size,similarity_user_3_sum/target_size,similarity_user_4_sum/target_size]
+    print("LEAST MISERY:")
+    songs_in_playlist_1 = 0
+    songs_in_playlist_2 = 0
+    songs_in_playlist_3 = 0
+    songs_in_playlist_4 = 0
+    for song in final_playlist_LEAST_MISERY:
+        if song in playlist_1:
+            songs_in_playlist_1+=1
+        if song in playlist_2:
+            songs_in_playlist_2+=1
+        if song in playlist_3:
+            songs_in_playlist_3+=1
+        if song in playlist_4:
+            songs_in_playlist_4+=1
+
+
+    print("Songs in final playlist from playlist 1: " + str(songs_in_playlist_1) + " from original " + str(len(playlist_1)))
+    print("Songs in final playlist from playlist 2: " + str(songs_in_playlist_2) + " from original " + str(len(playlist_2)))
+    print("Songs in final playlist from playlist 3: " + str(songs_in_playlist_3) + " from original " + str(len(playlist_3)))
+    print("Songs in final playlist from playlist 4: " + str(songs_in_playlist_4) + " from original " + str(len(playlist_4)))
+    print("Average similarity of chosen songs for user 1: ")
+
+    similarity_user_1_sum = 0
+    similarity_user_2_sum = 0
+    similarity_user_3_sum = 0
+    similarity_user_4_sum = 0
+    for song in final_playlist_LEAST_MISERY:
+        similarity_user_1_sum += ratings_playlist_1[song]
+        similarity_user_2_sum += ratings_playlist_2[song]
+        similarity_user_3_sum += ratings_playlist_3[song]
+        similarity_user_4_sum += ratings_playlist_4[song]
+
+    print("Average expected rating user 1: " + str(similarity_user_1_sum/target_size))
+    print("Average expected rating user 2: " + str(similarity_user_2_sum/target_size))
+    print("Average expected rating user 3: " + str(similarity_user_3_sum/target_size))
+    print("Average expected rating user 4: " + str(similarity_user_4_sum/target_size))
+    list_nums_LM = [songs_in_playlist_1, songs_in_playlist_2, songs_in_playlist_3, songs_in_playlist_4]
+    list_ratings_LM = [similarity_user_1_sum/target_size,similarity_user_2_sum/target_size,similarity_user_3_sum/target_size,similarity_user_4_sum/target_size]
+    return list_nums_AVG, list_ratings_AVG, list_nums_LM, list_ratings_LM
+
+
+sum_list_nums_AVG = [0,0,0,0]
+sum_list_ratings_AVG = [0,0,0,0]
+sum_list_nums_LM = [0,0,0,0]
+sum_list_ratings_LM = [0,0,0,0]
+num_samples = 1
+for i in range(num_samples):
+    indices = [1,2,12,14]#random.sample(range(0, len(PLAYLISTS)), 4)
+    list_nums_AVG, list_ratings_AVG, list_nums_LM, list_ratings_LM = aggregate(indices)
+    sum_list_nums_AVG = np.add(sum_list_nums_AVG, list_nums_AVG)
+    sum_list_ratings_AVG = np.add(sum_list_ratings_AVG, list_ratings_AVG)
+    sum_list_nums_LM = np.add(sum_list_nums_LM, list_nums_LM)
+    sum_list_ratings_LM = np.add(sum_list_ratings_LM, list_ratings_LM)
+sum_list_nums_AVG = np.divide(sum_list_nums_AVG, num_samples)
+sum_list_ratings_AVG = np.divide(sum_list_ratings_AVG, num_samples)
+sum_list_nums_LM = np.divide(sum_list_nums_LM, num_samples)
+sum_list_ratings_LM = np.divide(sum_list_ratings_LM, num_samples)
+
+print("AVERAGE:")
+print("Average number of songs per list:")
+print(sum_list_nums_AVG)
+print("Average rating of songs per list:")
+print(sum_list_ratings_AVG)
+print("LEAST MISERY:")
+print("Average number of songs per list: ")
+print(sum_list_nums_LM)
+print("Average rating of songs per list")
+print(sum_list_ratings_LM)
+
+
+print("AVERAGE vs LEAST MISERY: ")
+print(np.sum(sum_list_ratings_AVG))
+print(np.sum(sum_list_ratings_LM))
--- individual/CollaborativeFiltering.py
+++ individual/CollaborativeFiltering.py
@@ -14,16 +14,15 @@ start_time = time.time()
 print("Creating Ratings Dataframe...")
 with open("/data/Playlists_small.json", encoding="utf8") as f:
     PLAYLISTS = json.load(f)
-df_playlists = pd.read_json('../data/Playlists_small.json')
+df_playlists = pd.read_json('../data/Playlists_very_small.json')
 ratings_df = pd.read_csv('../data/ratings.csv')
-ratings_new_df = pd.read_csv('../data/ratings_new.csv')
 ratings_df.columns=['user', 'item']
 df_tracks = pd.read_json('../data/Tracks.json')
 print("Playlists Ratings created!")
 print("--- %s seconds ---" % (time.time() - start_time))
 
 #Lenskit CF approach
-num_recs = 10
+num_recs = 100
 k = 15
 item_item = ItemItem(k, min_nbrs=3, center=False, feedback='implicit', use_ratings=False)
 recsys = Recommender.adapt(item_item)
@@ -32,29 +31,20 @@ recsys.fit(ratings_df)
 similarity_sum_baseline = 0
 similarity_sum_recommendations = 0
 print("--- %s seconds ---" % (time.time() - start_time))
-for i in range(100):
+for i in range(9,10):
+    i=5
     #Print out results
     selected_user = i
     selected_tracks_useruser = recsys.recommend(selected_user, num_recs)
-    #Print the names of the recommended songs
-    # print(df_playlists.iloc[i]['name'])
-    # print(selected_tracks_useruser)
+    # Print the names of the recommended songs
+    print("Name: " + df_playlists.iloc[i]['name'])
+    print(selected_tracks_useruser.to_string())
     actual_recommendations = []
     #Print out explanation for each chosen song
     for j in range(len(selected_tracks_useruser)):
         # explanations.explanation_individual(selected_tracks_useruser.loc[j, 'item'], PLAYLISTS[i]['tracks'])
         actual_recommendations.append(selected_tracks_useruser.loc[j, 'item'])
-    baseline_recommendations = utils.get_top_x_random_popular_uris()
-    playlist = PLAYLISTS[i]
-    baseline_similarity = cosine_similarity.compute_playlist_list_similarity(playlist, baseline_recommendations)
-    print("Baseline similarity: " + str(baseline_similarity))
-    similarity_sum_baseline += baseline_similarity
-    recommendation_similarity = cosine_similarity.compute_playlist_list_similarity(playlist, actual_recommendations)
-    print("Recommendation similarity: " + str(recommendation_similarity))
-    similarity_sum_recommendations += recommendation_similarity
 
-print("Baseline: "+ str(similarity_sum_baseline/100))
-print("Recommendation: "+ str(similarity_sum_recommendations/100))
 
 def similarity_evaluation(i, actual_recommendations):
     baseline_recommendations = utils.get_top_x_random_popular_uris()
@@ -63,4 +53,3 @@ def similarity_evaluation(i, actual_recommendations):
     print("Baseline similarity: " + str(baseline_similarity))
     recommendation_similarity = cosine_similarity.compute_playlist_list_similarity(playlist, actual_recommendations)
     print("Recommendation similarity: " + str(cosine_similarity.compute_playlist_list_similarity(playlist, actual_recommendations)))
-
